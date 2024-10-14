fff7b961cd6d9e3b5da7d74d5b83de7012fbed0a
yylunxie <yylunxie@gmail.com>
2024-10-14 15:57:54 +0000

    Update the host adress in app.py and update the dependency of Python in Pipfile.lock

--- leaflet_diagnosis/app.py
+++ leaflet_diagnosis/app.py
@@ -84,4 +84,4 @@ async def predict(files: List[UploadFile] = File(...)):
 # To run the app using uvicorn
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="cv.plantman.toolmenlab.bime.ntu.edu.tw", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
